01d05d4185e803423f2e1ea0ece19cf277d1f596
Athamantis <helle@grasmo.no>
2024-06-14 15:46:33 +0200

    Fix array for board pieces

--- wizzardchess/src/main.rs
+++ wizzardchess/src/main.rs
@@ -1,3 +1,46 @@
+
+
+
+// enum a posibility of pieces. 
+enum BoardPieces {
+	pawn, 
+	bishop,
+	knight,
+	rook, 
+	queen,
+	king
+}
+
+
+struct PosistionHolder {
+	row: u8, 
+	colum: u8, 
+}
+
+// enum piece 
+// color, type piece, life, placement
+struct Pice {
+	posision: posistion_holder,
+	colour: board_pieces,
+}
+
+static mut WhitePieces: [Option<Pice>; 16] = [None; 16];
+
+// array with pieces, color and placement and if alive or not,
+void MakeArrayWithPieces(){
+	for i in (1..8){
+		
+	}
+}
+
+void AssignPlaceBoardpiecesArray (){
+	
+}
+
+
+// chessboard array whit the placement of the bricks. 
+
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +50,6 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+
 }
