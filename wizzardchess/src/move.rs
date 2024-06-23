4c8f0458b387fe5415ac5edc312ef56c7c603ac4
Athamantis <helle@grasmo.no>
2024-06-23 14:24:44 +0200

    make remove pieces

--- wizzardchess/src/move.rs
+++ wizzardchess/src/move.rs
@@ -1,4 +1,5 @@
 use crate::pieces::{set_up_board, BLACK_PIECES, WHITE_PIECES};
+use std::{ops::DerefMut, sync::{Mutex, MutexGuard}};
 
 
 fn move_position(inserted_piece: Piece, new_position: Posistion){
@@ -9,12 +10,23 @@ fn move_position(inserted_piece: Piece, new_position: Posistion){
     inserted_piece::posision::rank = new_position::rank;  
 }
 
+// Check if position is taken
+fn check_position(position: Posistion) -> Option<Piece>{
 
-fn check_position(){
+    if (){
+
+    }
+}
+
+
+fn piece_position_in_array(piece: Piece, mut array: MutexGuard<[Option<Piece>;16]>) -> u8 {
 
 }
 
-fn remove_piece(){
+// Takes inn the array where the piece shall be removed from and the position of the piece in the array.
+fn remove_piece(mut array: MutexGuard<[Option<Piece>;16]>, array_position: u8){
+    let array_pieces: &mut [Option<Piece>; 16] = array.deref_mut();
 
+    array_pieces[array_position] = None; 
 }
 
